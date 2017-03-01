a3e4ce26ab59c5149b2197a3d6b2ffda2aae0c81
Johan Gorter <johan-gorter@users.noreply.github.com>
2017-03-01 19:29:04 +0100

    before the break, adding elements

--- src/example.ts
+++ src/example.ts
@@ -1,9 +1,29 @@
-import {h, append} from './mini-vdom';
+import {h, append, Projector} from './mini-vdom';
+
+let projector: Projector;
+
+let todos = ['Buy Milk', 'Go vote'];
+
+let handleKeyDown = (evt: KeyboardEvent) => {
+  if (evt.which === 13) {
+    let inputElement = evt.target as HTMLInputElement
+    todos.push(inputElement.value);
+    inputElement.value = '';
+    projector.scheduleRender();
+  }
+}
 
 let render = () => {
-  return h('Hello...');
+  return h('div', {}, [
+    h('ul', {}, todos.map(todo => {
+      return h('li', {}, [
+        h('span', {}, todo)
+      ])
+    })),
+    h('input', {onkeydown: handleKeyDown})
+  ]);
 };
 
 document.addEventListener('DOMContentLoaded', () => {
-  let projector = append(document.body, render);
+  projector = append(document.body, render);
 });
--- src/mini-vdom.ts
+++ src/mini-vdom.ts
@@ -1,44 +1,91 @@
-export interface VNode {
-  content: string;
+export interface Projector {
+  scheduleRender: () => void;
 }
 
-export let h = (content: string): VNode => {
+export interface VNodeProperties {
+  onkeydown?: (evt: KeyboardEvent) => void;
+}
+
+export interface VNode {
+  tagName: string;
+  properties: VNodeProperties;
+  content: string | VNode[] | undefined;
+  element?: HTMLElement;
+}
+
+export let h = (tagName: string, properties: VNodeProperties, content?: string | VNode[]): VNode => {
   return {
-    content: content
+    tagName, properties, content
   };
 }
 
-export let append = (element: HTMLElement, render: () => VNode) => {
-
-  // Temporary dummy implementation
-  element.textContent = render().content;
-
-  return {};
+export let append = (element: HTMLElement, render: () => VNode): Projector => {
+  let lastVnodeTree = render();
+  create(lastVnodeTree);
+  element.appendChild(lastVnodeTree.element!);
+  return {
+    scheduleRender: () => {
+      requestAnimationFrame(() => {
+        let currentVNodeTree = render();
+        diffAndPatch(currentVNodeTree, lastVnodeTree);
+        lastVnodeTree = currentVNodeTree;
+      });
+    }
+  };
 }
 
+let create = (vnode: VNode) => {
+  let result = document.createElement(vnode.tagName);
+  vnode.element = result;
+  if (typeof vnode.content === 'string') {
+    result.textContent = vnode.content;
+  } else if (vnode.content !== undefined) {
+    vnode.content.forEach(childNode => {
+      create(childNode);
+      result.appendChild(childNode.element!);
+    });
+  }
+  if (vnode.properties.onkeydown) {
+    result.onkeydown = vnode.properties.onkeydown;
+  }
+}
 
+let diffAndPatch = (newTree: VNode, oldTree: VNode) => {
+  // Adopt previous domNode
+  let element = newTree.element = oldTree.element!;
 
+  // handle text content
+  if (typeof newTree.content === 'string') {
+    if (oldTree.content !== newTree.content) {
+      element.textContent = newTree.content;
+    }
+  } else if (Array.isArray(newTree.content)) {
+    if (!Array.isArray(oldTree.content)) {
+      throw new Error('Not supported');
+    }
+    let oldChildren = oldTree.content;
+    let newChildren = newTree.content;
+    let oldIndex = 0;
+    let newIndex = 0;
+    while (newIndex < newChildren.length) {
+      let oldChild = oldChildren[oldIndex];
+      let newChild = newChildren[newIndex];
 
+      // Same
+      if (same(newChild, oldChild)) {
+        diffAndPatch(newChild, oldChild);
+        oldIndex ++;
+      } else {
+        // TODO: Different, nodes were removed
 
-// Skeleton of the function that we are going to use.
-// let diffAndPatch = (newTree: VNode, oldTree: VNode) => {
-//   // Adopt previous domNode
-//
-//   // handle text content
-//
-//   let oldChildren = oldTree.content;
-//     let newChildren = newTree.content;
-//     let oldIndex = 0;
-//     let newIndex = 0;
-//     while (newIndex < newChildren.length) {
-//       let oldChild = oldChildren[oldIndex];
-//       let newChild = newChildren[newIndex];
-//
-//       // Same
-//
-//       // Different, nodes were removed
-//
-//       // Different, new node was inserted
-//     }
-//     // remove remaining nodes that are no longer present
-// };
+        // Different, new node was inserted
+        create(newChild);
+        element.insertBefore(newChild.element!, oldChild ? oldChild.element! : null);
+      }
+      newIndex ++;
+    }
+    // remove remaining nodes that are no longer present
+  }
+};
+
+let same = (newChild: VNode, oldChild: VNode | undefined) => oldChild && newChild.tagName === oldChild.tagName;
